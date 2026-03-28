9c663353a6e6ed0cc6868c923eee03d2cdfdd1ef
andres-cruz-zapata <andres.cruz18@upr.edu>
2026-03-27 23:33:36 -0400

    Added power up

--- ultimate_game_jam_2026/scripts/player.gd
+++ ultimate_game_jam_2026/scripts/player.gd
@@ -3,7 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 90.0
 const JUMP_VELOCITY = -200.0
+@onready var attackArea = $PhoneAttackArea
 
+var gotPhonePowerUp = false
+var hasPhone = false
+
+func _ready() -> void:
+	attackArea.monitoring = false
+	attackArea.visible = false
+	attackArea.get_node("CollisionShape2D").disabled = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,10 +29,51 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 		if Input.is_action_just_pressed("Dash"):
 			velocity.x = direction * (SPEED*15)
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if Input.is_action_just_pressed("Dash"):
 			velocity.x = move_toward(velocity.x,0,SPEED*15)
 
+	if Input.is_action_just_pressed("Power_Use") and hasPhone:
+		print("Attack")
+		attack(direction)
+
+	if gotPhonePowerUp:
+		# Changes the shape of the hitbox of the player to a rectangle; useful when turning into a phone
+		# var shape_node = get_node("CollisionShape2D")
+
+		# var new_shape = RectangleShape2D.new()
+		# new_shape.size = Vector2(50, 20)
+
+		# shape_node.set_deferred("shape", new_shape)
+
+		# Temporary differences
+		scale = Vector2(2, 2)
+
+		gotPhonePowerUp = false
+		hasPhone = true
+		print("Changed shape")
+
+
+
 
 	move_and_slide()
+
+
+func attack(direction: float) -> void:
+	var size = ($CollisionShape2D.shape as CircleShape2D).radius
+	var offset = Vector2(2 * size, -size)
+
+	if direction < 0:
+		offset.x = -offset.x
+
+	attackArea.position = offset
+
+	attackArea.monitoring = true
+	attackArea.visible = true
+	attackArea.get_node("CollisionShape2D").disabled = false
+	await get_tree().create_timer(0.2).timeout
+	attackArea.monitoring = false
+	attackArea.visible = false
+	attackArea.get_node("CollisionShape2D").disabled = true
