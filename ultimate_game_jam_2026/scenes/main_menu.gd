8c32a3831e59ac9e15483807f3cd39edda33eb6c
TomasGomez04 <tomasegom@gmail.com>
2026-03-29 20:25:16 -0400

    initial menu button changes scene

---
+++ ultimate_game_jam_2026/scenes/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+const levelScene = "res://scenes/Main.tscn"
+var change_scene = preload(levelScene)
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_packed(change_scene)
+	pass # Replace with function body.
