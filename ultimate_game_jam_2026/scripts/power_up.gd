34fdad82a0353b013fb3e1b1637d2ad8c04b64ed
andres-cruz-zapata <andres.cruz18@upr.edu>
2026-03-30 12:39:34 -0400

    level 2

---
+++ ultimate_game_jam_2026/scripts/power_up.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+#--------SIGNALS--------#
+
+func _on_body_entered(body: Node2D) -> void:
+	body.gotPhonePowerUp = true
+	queue_free()
+	print("Got Power Up")
+	pass # Replace with function body.
